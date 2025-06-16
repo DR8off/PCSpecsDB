df8d4ae1a22f450090eb208bd052ae0e028080d1
Zeorum <91529211+Zeorum@users.noreply.github.com>
2025-06-16 09:38:10 +0300

    Update powerunits.ts

--- powerunits.ts
+++ powerunits.ts
@@ -1,5 +1,34 @@
-const powerunits = [
-    { name: 'nameOfThisUnit', brand: 'brandName', power_w: 1000, }
-]
+// Импорт изображений блоков питания
+import corsair_rm1000x from "./assets/powerunits/corsair_rm1000x.webp";
+import bequiet_purepower11 from "./assets/powerunits/bequiet_purepower11.webp";
+import seasonic_focus850 from "./assets/powerunits/seasonic_tx850.webp";
+import deepcool_pk650d from "./assets/powerunits/deepcool_pk650d.webp";
 
-export default powerunits
+const powerunits = [
+  {
+    name: "Corsair RM1000x (2021)",
+    brand: "Corsair",
+    power_w: 1000,
+    image: corsair_rm1000x,
+  },
+  {
+    name: "be quiet! Pure Power 11 FM 750W",
+    brand: "be quiet!",
+    power_w: 750,
+    image: bequiet_purepower11,
+  },
+  {
+    name: "Seasonic PRIME TX-850",
+    brand: "Seasonic",
+    power_w: 850,
+    image: seasonic_focus850,
+  },
+  {
+    name: "DeepCool PK650D",
+    brand: "DeepCool",
+    power_w: 650,
+    image: deepcool_pk650d,
+  },
+];
+
+export default powerunits;
